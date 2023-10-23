aa4294088db50f1a974722649c4cf00694f1ff2f
elianap <eliana.p1993@gmail.com>
2023-10-23 14:46:15 +0200

    add pattern processor: sv, gsv, redundancy pruning

---
+++ divexplorer/pattern_processor.py
@@ -0,0 +1,231 @@
+# These fields are available in the patterns dataframe and we always want to keep them
+BASE_COLUMNS = ["support", "itemsets", "length", "support_count"]
+
+
+def sort_pattern(x, abbreviations={}):
+    """Sort a pattern and abbreviate by replacing the abbreviations"""
+    x = list(x)
+    x.sort()
+    x = ", ".join(x)
+    for k, v in abbreviations.items():
+        x = x.replace(k, v)
+    return x
+
+
+def abbreviate_dictionary(pattern_value, abbreviations):
+    # Shapley values (dict) as input
+    return {
+        frozenset([sort_pattern(k, abbreviations=abbreviations)]): v
+        for k, v in pattern_value.items()
+    }
+
+
+class DivergencePatternProcessor:
+    def __init__(self, patterns, metric):
+        """
+        :param patterns: dataframe patterns
+        :param metric: metric used for divergence
+        """
+        # We only want to keep the columns that are relevant to the metric + the base columns
+        self.patterns = patterns[
+            BASE_COLUMNS + [metric, f"{metric}_div", f"{metric}_t"]
+        ]
+        self.metric = metric
+        self.dict_len_pattern_divergence = (
+            self.dict_len_pattern_divergence_representation()
+        )
+
+    def dict_len_pattern_divergence_representation(self):
+        """
+        Define an intermediate representation of the patterns dataframe in the form
+        Len itemset -> {pattern: divergence}
+        :param metric: metric used for divergence
+        """
+        patterns = self.patterns
+        metric = self.metric + "_div"
+        d = patterns[["itemsets", metric]].set_index("itemsets").to_dict("index")
+        return {
+            k: {k1: v[metric] for k1, v in d.items() if k == len(k1)}
+            for k in range(0, max(patterns["length"] + 1))
+        }
+
+    def shapley_value(self, pattern=None, row_idx=None):
+        assert (
+            pattern is None or row_idx is None
+        ), "Either pattern or row_idx must be provided"
+        assert (
+            pattern is not None or row_idx is not None
+        ), "Either pattern or row_idx must be provided"
+
+        if row_idx is not None:
+            pattern = self.patterns.iloc[row_idx]["itemsets"]
+        from divexplorer.shapley_value import compute_shapley_value
+
+        return compute_shapley_value(pattern, self.dict_len_pattern_divergence)
+
+    def plot_shapley_value(
+        self,
+        pattern=None,
+        row_idx=None,
+        shapley_values=None,
+        figsize=(4, 3),
+        abbreviations={},
+        sort_by_value=True,
+        height=0.5,
+        linewidth=0.8,
+        labelsize=10,
+        title="",
+        x_label="",
+        name_fig=None,
+        save_fig=False,
+        show_figure=True,
+    ):
+        """
+        Plot the Shapley value of a pattern
+        Args:
+        Specify either pattern or row_idx or shapley_value
+        pattern (frozen set): list of items
+        row_idx (int): row index of the pattern in the patterns dataframe
+        shapley_values (dict): dictionary of pattern scores: {pattern: score}
+        figsize (tuple): figure size
+        abbreviations (dict): dictionary of abbreviations to replace in the patterns - for visualization purposes
+        sort_by_value (bool): sort the Shapley values by value
+        height (float): height of the bars
+        linewidth (float): width of the bar border
+        labelsize (int): size of the labels
+        title (str): title of the plot
+
+        """
+
+        assert (
+            pattern is None or row_idx is None or shapley_values is None
+        ), "Either pattern or row_idx or shapley_value must be provided"
+
+        assert (
+            pattern is not None or row_idx is not None or shapley_values is not None
+        ), "Either pattern or row_idx or shapley_value must be provided"
+
+        if pattern is not None or row_idx is not None:
+            shapley_values = self.shapley_value(pattern=pattern, row_idx=row_idx)
+
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots(1, 1, figsize=figsize, dpi=100)
+
+        if abbreviations:
+            shapley_values = abbreviate_dictionary(shapley_values, abbreviations)
+
+        # Dictionary of item in string format and value
+        shapleyv_plot = {str(",".join(list(k))): v for k, v in shapley_values.items()}
+
+        if sort_by_value:
+            shapleyv_plot = {
+                k: v for k, v in sorted(shapleyv_plot.items(), key=lambda item: item[1])
+            }
+
+        ax.barh(
+            range(len(shapleyv_plot)),
+            shapleyv_plot.values(),
+            height=height,
+            align="center",
+            color="#7CBACB",
+            linewidth=linewidth,
+            edgecolor="#0C4A5B",
+        )
+
+        ax.set_yticks(range(len(shapleyv_plot)), minor=False)
+        ax.set_yticklabels(list(shapleyv_plot.keys()), minor=False)
+        ax.tick_params(axis="y", labelsize=labelsize)
+        ax.tick_params(axis="x", labelsize=labelsize)
+        ax.set_title(title, fontsize=labelsize)
+
+        ax.set_xlabel(x_label, size=labelsize)
+
+        if save_fig:
+            name_fig = "shapley_value.pdf" if name_fig is None else name_fig
+
+            plt.savefig(
+                f"{name_fig}",
+                bbox_inches="tight",
+                pad=0.05,
+                facecolor="white",
+                transparent=False,
+            )
+        if show_figure:
+            plt.show()
+            plt.close()
+
+    def redundancy_pruning(self, th_redundancy):
+        """
+        Prune the patterns that are redundant with respect to the divergence
+        Args:
+            th_redundancy (float): threshold for redundancy
+        Returns:
+            (pd.DataFrame) patterns without redundancy
+        """
+        patterns_divergence = self.dict_len_pattern_divergence
+        redundants = []
+        for patterns_len_p in patterns_divergence.values():
+            for pattern in patterns_len_p:
+                for item in pattern:
+                    itemset_minus_item = pattern - frozenset([item])
+                    v_itemset_minus_item = patterns_divergence[len(itemset_minus_item)][
+                        itemset_minus_item
+                    ]
+
+                    v_pattern = patterns_divergence[len(pattern)][pattern]
+                    if abs(v_pattern - v_itemset_minus_item) <= th_redundancy:
+                        redundants.append(pattern)
+        patterns_not_red = self.patterns.loc[
+            self.patterns.itemsets.isin(redundants) == False
+        ]
+        return patterns_not_red
+
+    def get_patterns(self, th_redundancy=None, sort_by_divergence=True):
+        """
+        Return the patterns
+        Args:
+            th_redundancy (float): threshold for redundancy - if None, no redundancy pruning
+            sort_by_divergence (bool): sort the patterns by divergence
+        Returns:
+            (pd.DataFrame) patterns with their divergence
+
+        """
+        if th_redundancy is None:
+            patterns = self.patterns
+        else:
+            patterns = self.redundancy_pruning(th_redundancy)
+        if sort_by_divergence:
+            return patterns.sort_values(by=[f"{self.metric}_div"], ascending=False)
+        else:
+            return patterns
+
+    def global_shapley_value(self):
+        # Get 1-itemsets
+        items = [item for item in self.dict_len_pattern_divergence[1].keys()]
+
+        # Get attributes of 1-itemsets
+        attributes = list(set([list(item)[0].split("=")[0] for item in items]))
+
+        # Get cardinality of attributes
+
+        from collections import Counter
+
+        # Get the cardinality of each attribute - Number of items with that attribute
+        cardinality_attributes = dict(
+            Counter([list(item)[0].split("=")[0] for item in items])
+        )
+
+        global_shapley = {}
+        for item in items:
+            # Compute Global Shapley value of item
+            from divexplorer.shapley_value import global_itemset_divergence
+
+            global_shapley[item] = global_itemset_divergence(
+                item,
+                self.dict_len_pattern_divergence,
+                attributes,
+                cardinality_attributes,
+            )
+
+        return global_shapley
---
+++ divexplorer/shapley_value.py
@@ -0,0 +1,145 @@
+from itertools import chain, combinations
+
+
+def powerset(iterable):
+    s = list(iterable)
+    return [
+        frozenset(i)
+        for i in chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+    ]
+
+
+def compute_shapley_value_item(item, pattern, powerset_pattern, item_score):
+    # Get all subsets of the pattern (powerset) where item is present
+    subsets_item_i = [s for s in powerset_pattern if item.issubset(s)]
+
+    # Compute the delta score for each subset containing the item
+    deltas_item = {}
+
+    for subset in subsets_item_i:
+        subset_minus_item = subset - item
+
+        # Delta(s) = v(s) - v(s \ {i})
+        # The delta score is the score of the subset containing the item minus the score of the subset without the item
+        deltas_item[subset] = (
+            item_score[len(subset)][subset]
+            - item_score[len(subset_minus_item)][subset_minus_item]
+        )
+
+    def weight_delta_score(s, n):
+        """Weigh the delta scores
+        s = |S| = len(subset) without the item
+        n = |N| = len(pattern)
+        """
+        #  |S!|(|N| - |S| - 1)! / |N|!
+        import math
+
+        return (math.factorial(s) * math.factorial(n - s - 1)) / math.factorial(n)
+
+    return sum(
+        [  # We weight the delta score by |S!|(|N| - |S| - 1)! / |N|! where |S| = len(subset) without the item (len(k) - 1) and |N| = len(pattern)
+            weight_delta_score(len(k) - 1, len(pattern)) * v
+            for k, v in deltas_item.items()
+        ]
+    )
+
+
+def compute_shapley_value(pattern, item_score):
+    """Compute the Shapley value of a subset of items
+    Args:
+        pattern (frozen set): list of items
+        item_score (dict): dictionary of pattern scores: len(pattern) -> {pattern: score}
+    """
+
+    # Get all subsets of the pattern (powerset) - 2^N with N = len(pattern)
+    powerset_pattern = powerset(pattern)
+
+    # Initialize the Shapley value dictionary
+    shapley_value = {}
+
+    # For each item in the pattern
+    for item_i in [frozenset([i]) for i in pattern]:
+        # Compute the Shapley value for each item
+        shapley_value[item_i] = compute_shapley_value_item(
+            item_i, pattern, powerset_pattern, item_score
+        )
+    return shapley_value
+
+
+#### Global Shapley Value
+
+
+import numpy as np
+
+
+def attribute(item):
+    """Returns the attribute of an item.
+    Args:
+        item (str): item in the form of 'attribute=value'
+    Returns:
+        str: attribute of the item
+    """
+    return item.split("=")[0]
+
+
+def plus(f1, f2):
+    return frozenset(list(f1) + list(f2))
+
+
+def weight_factor_global_shapley_value(lB, lA, lI, prod_mb):
+    """Returns the weight factor for the global shapley value."""
+
+    # \frac{|B|! (|A|-|B|-|I|)!} {|A|! \prod_{b \in B \cup attr(I)} m_b}
+    import math
+
+    return (math.factorial(lB) * math.factorial(lA - lB - lI)) / (
+        math.factorial(lA) * (prod_mb)
+    )
+
+
+def global_itemset_divergence(I, scores_l, attributes, cardinality_attributes):
+    """Returns the global divergence of an itemset.
+    Args:
+        I (set): itemset
+        scores_l (dict): dictionary of len itemset, itemsets and their divergence
+        attributes (list): list of attributes (from frequent items)
+        card_map (dict): dictionary of attributes and their cardinality, i.e., how many values they can take (from frequent items)
+    """
+    # Get attributes different from the attribute of the item
+    # These are our set B
+    Bs = set(attributes) - {attribute(i) for i in I}
+
+    # Get itemsets whose attributes are in B
+    # These are our set I_B
+    I_Bs = [
+        pattern_i
+        for len_patterns in scores_l
+        for pattern_i in scores_l[len_patterns]
+        if [item_i for item_i in pattern_i if attribute(item_i) not in Bs] == []
+    ]
+
+    # Initialize
+    global_shapley_itemset = 0
+    # J \in I_B
+    for J in I_Bs:
+        JI = plus(J, I)
+
+        if len(JI) in scores_l and JI in scores_l[len(JI)]:
+            # Get the attributes in J
+            B = [attribute(i) for i in J]
+
+            # Get the attributes in J \cup I
+            attr_BI = B + [attribute(i) for i in I]
+
+            prod_mb = np.prod([cardinality_attributes[i] for i in attr_BI])
+
+            w = weight_factor_global_shapley_value(
+                len(B), len(attributes), len(I), prod_mb
+            )
+            # Accumulate the global shapley value
+            global_shapley_itemset = (
+                global_shapley_itemset
+                + (scores_l[len(JI)][JI] - scores_l[len(J)][J]) * w
+            )
+
+    return global_shapley_itemset
